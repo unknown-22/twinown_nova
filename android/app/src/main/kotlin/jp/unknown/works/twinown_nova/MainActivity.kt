1aa9727c2904d2b08cbcc65eb6b9788890c6887c
unknown22 <sora.rr.d+unknown22@gmail.com>
2019-09-23 02:47:23 +0900

    init

---
+++ android/app/src/main/kotlin/jp/unknown/works/twinown_nova/MainActivity.kt
@@ -0,0 +1,13 @@
+package jp.unknown.works.twinown_nova
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
